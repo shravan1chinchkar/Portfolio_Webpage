60c57b20eaa7e8489c9999c92578ae40656a58ec
Shravan <shravan009chinchkar100@gmail.com>
2024-01-27 21:08:47 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+let aboutlink=document.getElementsByClassName("about-link");
+let aboutnavcontent=document.getElementsByClassName("aboutnavcontent");
+
+function openContent(tabname){
+    for (const aboutlinks of aboutlink) {
+        aboutlinks.classList.remove("active-link")
+    }
+    for (const aboutnavcontents of aboutnavcontent) {
+        aboutnavcontents.classList.remove("active-content")
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-content"); 
+}
+
+let prjcard=document.getElementById("prjcardrow2");
+let seemmore=document.getElementById("seemore");
+let seeless=document.getElementById("seeless");
+
+
+
+function showMore(){
+    prjcard.classList.toggle("projectcardrow2");
+    seemmore.classList.add("seemorebutton");
+    seeless.classList.remove("seelessbutton");
+}
+
+function showLess(){
+    prjcard.classList.toggle("projectcardrow2");
+    seemmore.classList.remove("seemorebutton");
+    seeless.classList.add("seelessbutton");
+
+}
+
